0051138b3b1ea200db7cc63d6be0aa007e5b3cd5
Md. Omor Faruk <omorfaruk2010@gmail.com>
2020-05-11 01:44:16 +0600

    just made a resource controller named storeController

---
+++ app/Http/Controllers/storeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class storeController extends Controller
+{
+    public function index(){
+        echo "Hi this is from INDEX method and this is a get REQUEST";
+    }
+
+    public function create(){
+        echo "Hi this is from CREATE method and this is a get REQUEST";
+    }
+
+    public function show(){
+        echo "Hi this is from SHOW method and this is a get REQUEST";
+    }
+
+    public function edit(){
+        echo "Hi this is from EDIT method and this is a get REQUEST";
+    }
+}
